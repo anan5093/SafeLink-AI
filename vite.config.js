9cb71ed8c185c73e2552fc1446d018a3ddbafa4a
Ananad Raj <anand.22scse1011076@galgotiasuniversity.edu.in>
2026-01-01 23:27:44 +0530

    Add build configuration to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/SafeLink-AI/',   // 👈 EXACT repo name, case-sensitive
+  base: '/SafeLink-AI/',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
 })
+
 
