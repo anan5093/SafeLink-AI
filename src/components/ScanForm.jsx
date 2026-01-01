e030e3d2f53dc3db2b1d0102ba205777e0f3ca78
Ananad Raj <anand.22scse1011076@galgotiasuniversity.edu.in>
2026-01-01 22:49:47 +0530

    Update API URL to use environment variable

--- src/components/ScanForm.jsx
+++ src/components/ScanForm.jsx
@@ -13,7 +13,7 @@ export default function ScanForm() {
     setResult(null);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/predict`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ url }),
