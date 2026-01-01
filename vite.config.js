0fead05ee5397e0c3cafd216d54d18bc5cbf72d2
Ananad Raj <anand.22scse1011076@galgotiasuniversity.edu.in>
2026-01-01 21:51:19 +0530

    Remove server proxy configuration
    Removed server proxy configuration from Vite config.

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: '/SafeLink-AI/',   // 👈 MUST match repo name
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:8000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-});
+}),
